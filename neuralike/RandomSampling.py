da22063c4c3214562f870de0c6e57e863bd12ca4
igomezv <neorelativista@gmail.com>
2022-06-16 16:37:18 -0500

    cleaning RandomSampling class

--- neuralike/RandomSampling.py
+++ neuralike/RandomSampling.py
@@ -4,8 +4,7 @@ import time
 
 
 class RandomSampling:
-    # def __init__(self, like, means, cov, nrand=10, pool=None, files_path='randomsampling'):
-    def __init__(self, like, means, mins, maxs, nrand=10, files_path='randomsampling'):
+    def __init__(self, like_fn, samples, means, mins, maxs, nrand=10, files_path='randomsampling'):
         """
         Create a random samples in the parameter space and evaluate the likelihood in them.
         This is used to generate the training set for a neural network.
@@ -16,7 +15,8 @@ class RandomSampling:
         pars: list of Parameter objects
         nrand: number of random points in the parameter space. Default is 500
         """
-        self.like = like
+        self.like = like_fn
+        self.samples = samples
         self.means = means
         # self.dims = len(means)
         self.mins = mins
@@ -28,17 +28,7 @@ class RandomSampling:
         print("\nGenerating a random sample of points in the parameter space...")
 
     def make_sample(self):
-        # if not self.filesChecker():
-        # samples = np.random.multivariate_normal(self.means, 5e-3*self.cov, size=(self.nrand,))
-        # else:
-        #     print('Loading existing random_samples and likelihoods: {}'.format(self.files_path))
-        #     samples = np.load('{}_random_samples.npy'.format(self.files_path))
-        # print("cov, means, samples", np.shape(self.cov), np.shape(self.means), np.shape(samples))
-        # samples = np.zeros((self.nrand, self.dims))
-        # for i in range(self.dims):
-        d1 = np.abs(self.means-self.mins)
-        d2 = np.abs(self.means-self.maxs)
-        std = min(d2, d1)/2
+        std = np.std(self.samples, axis=0)/4
         samples = np.random.normal(loc=self.means, scale=std, size=(self.nrand, self.dims))
         print("Random samples in the parameter space generated!")
         return samples
@@ -58,14 +48,3 @@ class RandomSampling:
         tf = time.time() - t1
         print("Time of {} likelihood evaluations {:.4f} min".format(len(likes), tf/60))
         return samples, likes
-
-    # def filesChecker(self):
-    #     """
-    #     This method checks if the name of the random_samples.npy and likes.npy exists, if it already exists use it
-    #     """
-    #     if os.path.isfile('{}_random_samples.npy'.format(self.files_path)):
-    #         if os.path.isfile('{}_likes.npy'.format(self.files_path)):
-    #             return True
-    #     else:
-    #         return False
-
